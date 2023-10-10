8a01b419bee86d103b0e609a9d7e82f8937e9780
remouter <remouter@gmail.com>
2023-10-10 11:52:23 +0400

    init commit

---
+++ src/test/java/com/example/MyTest.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class MyTest {
+    public void testMyTest() {
+
+        System.out.println("###############");
+        System.out.println(System.getenv("HOST"));
+    }
+
+}
